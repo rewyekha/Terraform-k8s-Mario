ab6ea6c01935fdbe1ea81c894300309e18d4b256
Reyas Khan M <reyaskhan001@gmail.com>
2025-09-03 19:24:59 +0530

    Add Terraform configuration for AWS and Kubernetes providers

---
+++ providers.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.20"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+# The Kubernetes provider uses the output of the EKS cluster creation
+# to configure its authentication. This is a key pattern.
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_id
+}
